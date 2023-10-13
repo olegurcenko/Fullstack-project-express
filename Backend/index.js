1a69d6e2cb268ba3259c7368fd5ca782fbf16dc7
olegurcenko <olegurcenko4@gmail.com>
2023-10-13 19:33:30 +0200

    added and refactored frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import multer from 'multer'
+import cors from 'cors'
 import { registerValidator, loginValidator, postCreateValidator } from './validations/validations.js'
 import { UserController, PostController } from './controllers/index.js'
 import { handleErrors, checkAuth } from './utils/index.js'
@@ -30,6 +31,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/uploads', express.static('uploads'))
 
